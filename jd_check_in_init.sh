d3a9d2f866c638e8554fc9ebb050715e18f53993
mengyyy <mengyyy@users.noreply.github.com>
2017-09-12 17:21:34 +0800

    Create jd_check_in_init.sh
    辅助脚本

---
+++ jd_check_in_init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ps ax | grep jd_checkin.py | grep -v  grep | cut -c 1-6 | xargs kill -9
+wget -O jd_checkin.py https://raw.githubusercontent.com/mengyyy/learn_python/master/jd_checkin.py
+nohup python3 jd_checkin.py &>/dev/null &
+echo you should sent /jdc to your bot
