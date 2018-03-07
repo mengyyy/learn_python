778c21d06834bb84e489aedded9fd3de6ca064e5
mengyyy <mengyyy@users.noreply.github.com>
2018-03-07 15:41:10 +0800

    Update init.sh

--- jd_checkin/init.sh
+++ jd_checkin/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-apt-get install firefox-esr
+apt install firefox fonts-noto-cjk fonts-wqy-zenhei
+pip3 install -U logzero selenium
 pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
