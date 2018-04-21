835d14414855c450219394cd8b2275c10e1b5102
mengyyy <mengyyy@users.noreply.github.com>
2018-04-21 23:24:39 +0800

    Update pipUpdate.py

--- pipUpdate.py
+++ pipUpdate.py
@@ -1,25 +1,25 @@
 #! /usr/bin/python3
 # -*- coding:utf-8 -*-
-# pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  |
-# xargs -n1 pip3 install -U
 
 import subprocess
 import json
-import pprint
 import logzero
 
 logger = logzero.logger
 
 checkCMD = 'pip3 list --outdate --format=json'
 updateCMDPlan = 'pip3 install -U {}'
+outputLogPlan = '{name:%10s} | {version} --> {latest_version}'
 
 p = subprocess.Popen(checkCMD, stdout=subprocess.PIPE, shell=True)
 output, err = p.communicate()
 if not err:
-    poutput = pprint.pformat(json.loads(output.decode('utf-8')))
-    logger.info('new package | \n{}'.format(poutput))
-    packageList = [i['name'] for i in json.loads(output.decode('utf-8'))]
-    if packageList:
+    outputData = json.loads(output.decode('utf-8'))
+    packageList = []
+    for i in outputData:
+        logger.info(outputLogPlan.format(**i))
+        packageList.append(i['name'])
+    if len(packageList) > 0:
         updateCMD = updateCMDPlan.format(' '.join(packageList))
         p = subprocess.Popen(updateCMD, shell=True)
         output, err = p.communicate()
